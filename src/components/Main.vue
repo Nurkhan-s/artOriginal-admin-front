7304ef789ee2e63ffa2a640f5e2af8249ace3c84
Nurkhan Shakuali <nurkhanshakuali@Nurkhans-MacBook-Pro.local>
2023-08-10 16:29:51 +0600

    build: vue3 ts project

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <div>
+      hello there
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
